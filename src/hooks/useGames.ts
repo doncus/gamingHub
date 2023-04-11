691e7ff3b2639551e51291b44a8e7b8679bed320
Donc <dominic.handl@hotmail.com>
2023-04-11 12:33:36 +0200

    display platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,18 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  // An array of objects where each object has a property platform of type Platform
+  // due to the fetch results ...
+  parent_platforms: { platform: Platform }[];
 }
 interface FetchGamesResonse {
   count: number;
